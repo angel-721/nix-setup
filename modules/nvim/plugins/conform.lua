378ca728dd6509dc73e71bb04071527b305bb33d
angel-721 <angelvelasquez2002@gmail.com>
2024-06-01 19:41:10 -0600

    Update nvim plugins

--- modules/nvim/plugins/conform.lua
+++ modules/nvim/plugins/conform.lua
@@ -2,6 +2,8 @@ require("conform").setup({
   formatters_by_ft = {
     python = { "ruff_organize_imports", "ruff_format", "ruff_fix" },
     javascript = { { "prettierd", "prettier" } },
+    css = { { "prettierd", "prettier" } },
+    html = { { "prettierd", "prettier" } },
     astro = { { "prettierd", "prettier" } },
     nix = {"alejandra "},
     cpp = {"clang-format"} ,
--- modules/shell/sh.nix
+++ modules/shell/sh.nix
@@ -6,12 +6,12 @@
     enable = true;
     shellAliases = {
       ll = "ls -l";
+	  CHROME_PATH="/nix/store/qikp6aziwfxgl5agwmbxf4d1zwnjxkx1-user-environment/bin/google-chrome-stable";
 	  getpythonenv = "cp ~/.config/nixos/shell/python/shell.nix .";
 	  getnodeenv = "cp ~/.config/nixos/shell/web/shell.nix .";
 	  getcplusenv = "cp ~/.config/nixos/shell/cpp/shell.nix .";
 	  gonix = "cd ~/.config/nixos/";
 	  g = "google-chrome-stable";
-
 	  n = "nix-shell";
 	  v = "nvim";
 	  r = "yazi";
